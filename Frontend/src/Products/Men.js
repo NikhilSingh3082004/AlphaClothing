8229671477ce0a55a32e2c85b2ab67e1c15539d8
NikhilSingh3082004 <nikhilsingh118089@gmail.com>
2026-08-02 04:02:11 +0530

    Restructure project into separate Frontend and Backend folders

---
+++ Frontend/src/Products/Men.js
@@ -0,0 +1,87 @@
+
+export const menProducts = [
+    {
+        id: 1,
+        name: "Formal Shirt",
+        description: "Very Good Quality Shirt",
+        price: 799,
+        image: "https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/24075210/2023/8/4/655ca16f-f062-414b-972e-17bacc0ccf731691133431459-Louis-Philippe-Men-Shirts-7301691133431027-1.jpg"
+    },
+    {
+        id: 2,
+        name: "Formal Shirt",
+        description: "Very Good Quality Shirt",
+        price: 799,
+        image: "https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/13340290/2021/12/8/2f5aa8b9-fa51-469a-9d7a-f6b1c04a77cd1638962231655-INVICTUS-Men-Easy-Care-Blue--White--Self-Design-Formal-Shirt-1.jpg"
+    },
+    {
+        id: 3,
+        name: "Formal Shirt",
+        description: "Very Good Quality Shirt",
+        price: 799,
+        image: "https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/23906402/2023/9/11/6407a87e-94ea-4731-8150-1a824c668f8a1694428657504-Raymond-Men-Shirts-3671694428657127-1.jpg"
+    },
+    {
+        id: 4,
+        name: "Formal Shirt",
+        description: "Very Good Quality Shirt",
+        price: 799,
+        image: "https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/23092100/2023/5/26/b1e6e092-034e-4a48-827b-12273b895a1c1685089816622-Louis-Philippe-Men-Shirts-6411685089816167-1.jpg"
+    },
+    {
+        id: 5,
+        name: "Formal Shirt",
+        description: "Very Good Quality Shirt",
+        price: 799,
+        image: "https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/24075228/2023/8/2/3224dbcd-59ea-4a2d-a03d-c6d266a37d4e1690951624779-Louis-Philippe-Men-Shirts-6051690951624383-1.jpg"
+    },
+    {
+        id: 6,
+        name: "Formal Shirt",
+        description: "Very Good Quality Shirt",
+        price: 799,
+        image: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/22642794/2023/6/8/db6087a2-eb15-4596-9ebc-21972f1870f81686218121792-FUBAR-Modern-Spread-Collar-Cotton-Formal-Shirt-1161686218121-1.jpg"
+    },
+    {
+        id: 7,
+        name: "Formal Shirt",
+        description: "Very Good Quality Shirt",
+        price: 799,
+        image: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/22642784/2023/6/8/fdd98b4f-2f6f-4b1b-a0fe-719d3650cb301686217962737-FUBAR-Modern-Spread-Collar-Cotton-Formal-Shirt-5191686217962-1.jpg"
+    },
+    {
+        id: 8,
+        name: "Formal Shirt",
+        description: "Very Good Quality Shirt",
+        price: 799,
+        image: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/19092112/2022/7/25/7451e6af-cb1e-4b8f-bd48-7ad1d8c5c8bd1658745156589-Louis-Philippe-Men-Shirts-8751658745155997-1.jpg"
+    },
+    {
+        id: 9,
+        name: "Formal Shirt",
+        description: "Very Good Quality Shirt",
+        price: 799,
+        image: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/23558420/2023/8/4/023f3362-84e5-421e-add0-a8965979ae201691132303025-Louis-Philippe-Men-Shirts-1081691132302577-1.jpg"
+    },
+    {
+        id: 10,
+        name: "Formal Shirt",
+        description: "Very Good Quality Shirt",
+        price: 799,
+        image: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/24670748/2023/9/8/9ce0b364-e5ab-46e8-9216-8c2fdd0a0bf61694166034297-Louis-Philippe-Men-Shirts-3181694166033973-1.jpg"
+    },
+    {
+        id: 11,
+        name: "Formal Shirt",
+        description: "Very Good Quality Shirt",
+        price: 799,
+        image: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15193834/2021/8/19/f60472e2-871e-4f2c-b24a-08e9c2a4264e1629353922187SlimFitEasy-ironshirt1.jpg"
+    },
+    {
+        id: 12,
+        name: "Formal Shirt",
+        description: "Very Good Quality Shirt",
+        price: 799,
+        image: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/23692656/2023/8/4/4ae56d19-790f-498c-a6e6-f3a88ec9962d1691140878286-Peter-England-Men-Shirts-8971691140877819-1.jpg"
+    },
+]
